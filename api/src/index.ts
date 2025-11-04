bead819485215209626d5fd68a6e6c53ec47b709
keijop <76419484+keijop@users.noreply.github.com>
2025-11-04 21:56:21 +0200

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,7 @@
 import { app } from '@azure/functions';
-import { semanticMovieSearch } from './functions/semanticMovieSearch'; // Import the function
 
 app.setup({
   enableHttpStream: true,
 });
 
-app.http('semanticMovieSearch', semanticMovieSearch); // Now it's tied to HTTP request
 
